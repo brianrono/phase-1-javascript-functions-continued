ac790db06c149a44a817ef41e73e751ccaf5d3bb
brianrono <brianrono16@gmail.com>
2023-03-14 13:56:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun(){
+    console.log("This Saturday, I want to hike!");
+}saturdayFun();
+
+function saturdayFun(roskate){
+    if (roskate === undefined){
+        roskate = "roller-skate";
+}return (`This Saturday, I want to ${roskate}!`);
+}
+
+let mondayWork = function(office = "go to the office"){
+    return `This Monday, I will ${office}.`
+}
+
+function wrapAdjective(adjective){
+    return function wrap(text){
+        return `You are ${adjective}${text}${adjective}!`
+    }
+}
+
+let adjective = wrapAdjective ('||');
+let nest = adjective('a dedicated programmer');
+
+let emphatic = wrapAdjective('*')('a hard worker');
+let other = wrapAdjective('||')('a dedicated programmer');
+
+console.log(emphatic,'\n',other);
